08f550e66b9fd2ec940b381c31c7c9a79a81baae
bilderbuchi <bilderbuchi@phononoia.at>
2010-07-12 09:35:09 +0200

    Added the GPLv3 license as per stiwi's wishes.

--- ofxGui/src/ofxGuiGlobals.cpp
+++ ofxGui/src/ofxGuiGlobals.cpp
@@ -2,8 +2,20 @@
  *  ofxGuiGlobals.cpp
  *  openFrameworks
  *
- *  Created by Stefan Kirch on 18.06.08.
- *  Copyright 2008 alphakanal. All rights reserved.
+ *  Copyright 2008 alphakanal/Stefan Kirch.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
